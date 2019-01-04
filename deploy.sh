a62a329d3c92c4a027c54bebc2c37a140e0c3299
Bruno Vieira <mail@bmpvieira.com>
2019-01-04 23:33:42 +0000

    Update .travis.yml auto push to GitHub

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
   git config user.email "travis@stenci.la"
   git checkout -b updates
   git add .
-  git commit -m "Updated at ${date --utc +%FT%TZ}"
+  git commit -m "Updated at $(date --utc +%FT%TZ)"
   git push origin updates
 fi
